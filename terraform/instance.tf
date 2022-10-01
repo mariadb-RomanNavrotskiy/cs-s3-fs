81fe500d2a16595f2d8bd984cfa3c68757892347
mariadb-RomanNavrotskiy <roman.navrotskiy@mariadb.com>
2022-10-01 22:12:48 +0500

    cleanup

--- terraform/instance.tf
+++ terraform/instance.tf
@@ -15,15 +15,15 @@ resource "aws_instance" "instance" {
   user_data = data.template_file.user_data.rendered
 }
 
-resource "aws_instance" "minio" {
-  count                  = var.minio.count
-  instance_type          = var.minio.size
-  ami                    = var.instance.ami
-  subnet_id              = aws_subnet.subnet.id
-  vpc_security_group_ids = [aws_security_group.security-instance.id, aws_security_group.security-instance-to-efs.id]
-  key_name               = var.instance.key_name
-  root_block_device {
-    volume_size = var.instance.rootfs_size
-  }
-  user_data = data.template_file.user_data.rendered
-}
+#resource "aws_instance" "minio" {
+#  count                  = var.minio.count
+#  instance_type          = var.minio.size
+#  ami                    = var.instance.ami
+#  subnet_id              = aws_subnet.subnet.id
+#  vpc_security_group_ids = [aws_security_group.security-instance.id, aws_security_group.security-instance-to-efs.id]
+#  key_name               = var.instance.key_name
+#  root_block_device {
+#    volume_size = var.instance.rootfs_size
+#  }
+#  user_data = data.template_file.user_data.rendered
+#}
